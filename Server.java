80e1fefa13a8a289d4ee2ada936727c4fd40b44d
laosiaudi <laosiaudi@gmail.com>
2013-11-28 00:38:46 +0800

    some functions added

---
+++ Server.java
@@ -0,0 +1,76 @@
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class Server{
+	ArrayList<String>userlist = new ArrayList<String>();
+	/**
+	*used to handshake, just to send handshake message
+	*/
+	private void handshake(){
+
+	}
+
+	/**
+	*used to deal with user log in
+	*/
+	private void user_log_in(){
+
+	}
+
+	/**
+	*return message as status of logging in to user
+	*/
+	private String status(){
+
+	}
+
+	/**
+	*deal with send online userlist to user
+	*/
+	private void send_list(){
+
+	}
+
+	/**
+	*send new_user_status to other users
+	*/
+	private void update_user_list(){
+
+	}
+
+	/**
+	*deal with user log out
+	*/
+	private void user_log_out(){
+
+	}
+
+	/**
+	*deal with group talk
+	*/
+	private void csmessage(){
+
+	}
+
+	/**
+	*deal with user heart-beat
+	*/
+	private void keeo_beat(){
+
+	}
+	public static void main(String argv[]) throws Exception{
+		
+		String clientSentence;
+
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+		while(true){
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			outToClient.writeBytes(clientSentence);
+
+		}
+	}
+}
