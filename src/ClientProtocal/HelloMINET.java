872bee51fe5c1b452369ae3c16901eba9c251ed6
huangm-fat <444792006@qq.com>
2013-12-01 00:22:21 +0800

    delete ascii

--- src/ClientProtocal/HelloMINET.java
+++ src/ClientProtocal/HelloMINET.java
@@ -4,7 +4,6 @@ import java.net.*;
 public class HelloMINET extends Protocal {
 	String localIP;
 	public HelloMINET(String localIP){
-		StringTrans trans = new StringTrans();
-		super.content = trans.StringToAsciiString("MINET") + " " + trans.StringToAsciiString(localIP) + "\n";
+		super.content = "MINET" + " " + localIP + "\n";
 	}
 }
