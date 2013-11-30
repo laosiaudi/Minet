62e73c41cbce8ee6649f967e63a011f9311cfcd3
JinnieTsai <caijin11331007@gmail.com>
2013-11-30 21:26:38 +0800

    update

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -6,17 +6,30 @@ class ClientTest{
 		String sentence;
 		String modifiedSentence;
 		while (true){
-			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			Socket clientSocket = new Socket("127.0.0.1",6788);
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			sentence = inFromUser.readLine();
+			/*BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));*/
+			//Socket clientSocket = new Socket("127.0.0.1",6788);
+			//DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			//BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+			//sentence = inFromUser.readLine();
 			//sentence = "minet lll \ndddd dddd\n";
-            outToServer.writeBytes(sentence + '\n');
+            //outToServer.writeBytes(sentence + '\n');
 			//modifiedSentence = inFromServer.readLine();
 			//System.out.println("From Server: " + modifiedSentence);
 		    ///clientSocket.close();
-		    
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            Socket clientSocket = new Socket("127.0.0.1",7000);
+            
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+            String Test = inFromUser.readLine();
+
+            outToServer.writeBytes(Test + '\n');
+
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String Result = inFromServer.readLine();
+            System.out.println(Result);
 		}
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,49 +1,154 @@
 import java.net.*;
 import java.io.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Server{
-	ArrayList<String>userlist = new ArrayList<String>();
+    Map<String, String> userlist = new HashMap<String, String>();
+    String Status = "";
+	//ArrayList<String>userlist = new ArrayList<String>();
 	/**
 	*used to handshake, just to send handshake message
 	*/
-	private void handshake(){
+	private void handshake(String clientSentence){
+        Status = "";
 
+        Status = clientSentence.replace("MINET", "MIRO");
 	}
 
 	/**
 	*used to deal with user log in
 	*/
-	private void user_log_in(BufferedReader inFromClient){
+	private void user_log_in(String inFromClient, Socket connectionSocket){
+        String []options = inFromClient.split(" ");
+        
+        String User_Name = options[2].split("\n\r")[0];
+
+        String Port_Num = options[3].split("\n\r")[0];
+        String IP_Num = connectionSocket.getInetAddress().getHostAddress();
+
+        String User_Info = IP_Num + "," + Port_Num;
+        //System.out.println(User_Info);
+        Status = "";
+        if (userlist.get(User_Name) != null) {
+        	  String Request_Line = "CS1.0" + " " + "STATUS" + " " + "0" + "\n\r";
+              
+
+              String Message = "The user name already exists";
+              String Entity_Body = Message + "\n\r";
+
+              /**
+               * Set the time format
+               */
+              SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+              Date Current_Date = new Date(System.currentTimeMillis());
+              String DateStr = formatter.format(Current_Date);
+              /** The size of entity body
+               */
+              String Content_Length = String.valueOf(Entity_Body.getBytes().length);
+       
+              String Header_Line = "Date" + " " + DateStr + "Content-Length" + " " + Content_Length + "\n\r";
+
+              Status += Request_Line + Header_Line + "\n\r" + Entity_Body;
+        }
+           // return false;
+        else {
+            userlist.put(User_Name, User_Info);
+            String Request_Line = "CS1.0" + " " + "STATUS" + " " + "1" + "\n\r";
+
+            /**
+             * Set the time format
+             */
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            Date Current_Date = new Date(System.currentTimeMillis());
+            String DateStr = formatter.format(Current_Date);
+            String Header_Line = "Date" + " " + DateStr + "Content-Length" + " " + "0" + "\n\r";
+            System.out.println(DateStr);
+
+
+            /**
+             * If the user logs in successfully, then the entity body is set to be empty
+             */
+            String Entity_Body = "\n\r";
+
+            Status = Request_Line + Header_Line + "\n\r" + Entity_Body;
+
+            System.out.println(Status);
+            
+        }
 
 	}
 
-	/**
-	*return message as status of logging in to user
-	*/
-	private String status(){
-        return "";
-	}
-
+	
 	/**
 	*deal with send online userlist to user
 	*/
 	private void send_list(){
+        Status  = "";
+        String Request_Line = "CS1.0" + " " + "LIST" + "\n\r";
+
+        String Entity_Body = "";
+        /**
+         * Traverse the user list
+         */
+        for (String key : userlist.keySet()) {
+            String value = userlist.get(key);
+            String []temp = value.split(",");
+
+            Entity_Body += key + " " + temp[0] + " " + temp[1] + "\n\r";
+        }
+
+        Date Current_Date = new Date(System.currentTimeMillis());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String DateStr = formatter.format(Current_Date);
+
+        String Content_Length = String.valueOf(Entity_Body.getBytes().length);
+       // SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        String Header_Line = "Date" + " " + DateStr + "Content-Length" + Content_Length + "\n\r";
+        Status = Request_Line + Header_Line + "\n\r" + Entity_Body;
 
 	}
 
 	/**
 	*send new_user_status to other users
 	*/
-	private void update_user_list(){
+	private void update_user_list(String Name, int i){
+        Status = "";
 
-	}
+        String Request_Line = "";
+
+
+        Date Current_Date = new Date(System.currentTimeMillis());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String DateStr = formatter.format(Current_Date);
+
+        String Header_Line = "Date" + " " + DateStr + "Content-Length" + " " + "0" + "\n\r";
+
+        String Entity_Body = "\n\r";
+        if (i == 0) 
+            Request_Line = "CS1.0" + " " + "UPDATE" + " " + "0" + " " + Name + "\n\r";
+        else
+            Request_Line = "CS1.0" + " " + "UPDATE" + " " + "1" + " " + Name + "\n\r";
+
+        Status += Request_Line + Header_Line + Entity_Body;
+    }
+
+    private void get_list() {
+    }
 
 	/**
 	*deal with user log out
 	*/
-	private void user_log_out(){
-
+	private void user_log_out(String clientSentence){
+        String []options = clientSentence.split(" ");
+        String User_Name = options[2].split("\n\r")[0];
+        userlist.remove(User_Name);
+        update_user_list(User_Name,0);
 	}
 
 	/**
@@ -71,7 +176,7 @@ public class Server{
     */
 	private static int action(String message){
         String []options = message.split(" ");
-        if (options[0].equals("Minet"))
+        if (options[0].equals("MINET"))
             return 1;
         else{
             if (options[1].equals("LOGIN"))
@@ -99,16 +204,20 @@ public class Server{
                     int state = action(clientSentence);
                     switch (state) {
                         case 1:
-                            handshake();
+                            handshake(clientSentence);
+                            outToClient.writeBytes(Status + '\n');
                             break;
                         case 2:
-                            user_log_in(inFromClient);
+                            user_log_in(clientSentence, connectionSocket);
+                            outToClient.writeBytes(Status + '\n');
                             break;
                         case 3:
                             send_list();
+                            outToClient.writeBytes(Status + '\n');
                             break;
                         case 4:
-                            user_log_out();
+                            user_log_out(clientSentence);
+                            outToClient.writeBytes(Status + '\n');
                             break;
                         case 5:
                            csmessage();
@@ -129,7 +238,7 @@ public class Server{
 		
 		String clientSentence;
         Server server = new Server();
-		ServerSocket welcomeSocket = new ServerSocket(6788);
+		ServerSocket welcomeSocket = new ServerSocket(7000);
 		while(true){
 			Socket connectionSocket = welcomeSocket.accept();
             server.process(connectionSocket);
