c9fb7080e9a48d32955383a45211612974e2dd75
huangm-fat <444792006@qq.com>
2013-12-05 00:40:11 +0800

    Update HelloMINET.java

--- src/ClientProtocol/HelloMINET.java
+++ src/ClientProtocol/HelloMINET.java
@@ -4,6 +4,6 @@ import java.net.*;
 public class HelloMINET extends Protocol {
 	String localIP;
 	public HelloMINET(String localIP){
-		super.content = "MINET" + super.sp + localIP + super.crlf;
+		super.content = "MINET" + super.sp + localIP + super.crlf + "\n";
 	}
 }
